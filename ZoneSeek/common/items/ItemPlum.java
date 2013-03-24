93defe45884b2b437081ea202a9c07b3e65559dc
LemonAfterburn <zachary.auzenne@gmail.com>
2013-03-24 01:54:32 -0300

    Update ItemPlum.java

--- ZoneSeek/common/items/ItemPlum.java
+++ ZoneSeek/common/items/ItemPlum.java
@@ -14,9 +14,9 @@ public class ItemPlum extends ItemFood{
 	}
 	@Override
 	@SideOnly(Side.CLIENT)
-	public void func_94581_a(IconRegister par1IconRegister)
+	public void updateIcons(IconRegister par1IconRegister)
 	{
-		this.iconIndex = par1IconRegister.func_94245_a("zoneseek:plum");
+		this.iconIndex = par1IconRegister.registerIcon("zoneseek:plum");
 	}
 
 }
