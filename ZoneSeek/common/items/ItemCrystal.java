0815da36a241f137acf61122f972fbbbfeb92df6
Adam Bahr <thebasedrebel@gmail.com>
2013-03-24 00:51:49 -0400

    Fixed

--- ZoneSeek/common/items/ItemCrystal.java
+++ ZoneSeek/common/items/ItemCrystal.java
@@ -1,12 +1,25 @@
 package ZoneSeek.common.items;
 
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.item.Item;
 
 public class ItemCrystal extends Item{
+	
+	private String textureName;
 
-	public ItemCrystal(int id) {
+	public ItemCrystal(int id, String tex) {
 		super(id);
+		this.textureName = tex;
+	}
+	public String getTextureName(){
+		return this.textureName;
+	}
+	
+	public void updateIcons(IconRegister reg){
+		this.iconIndex = reg.registerIcon("zoneseek:" + this.getTextureName());
 	}
 
 }
