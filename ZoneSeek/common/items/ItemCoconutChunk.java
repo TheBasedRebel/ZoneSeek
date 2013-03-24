ffd779a6dce2cde34706d17f5021f75424849b88
LemonAfterburn <zachary.auzenne@gmail.com>
2013-03-24 01:39:28 -0300

    Update ItemCoconutChunk.java

--- ZoneSeek/common/items/ItemCoconutChunk.java
+++ ZoneSeek/common/items/ItemCoconutChunk.java
@@ -14,9 +14,9 @@ public class ItemCoconutChunk extends ItemFood{
 	}
 	@Override
 	@SideOnly(Side.CLIENT)
-	public void func_94581_a(IconRegister par1IconRegister)
+	public void registerIcons(IconRegister par1IconRegister)
 	{
-		this.iconIndex = par1IconRegister.func_94245_a("zoneseek:coconutchunk");
+		this.iconIndex = par1IconRegister.registerIcon("zoneseek:coconutchunk");
 	}
 
 }
