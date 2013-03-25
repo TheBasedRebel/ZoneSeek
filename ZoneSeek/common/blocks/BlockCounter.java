c0ecd877eea3fc430f9992057472e579f6beecce
Adam Bahr <thebasedrebel@gmail.com>
2013-03-24 22:43:55 -0400

    1.5.1 texture update

---
+++ ZoneSeek/common/blocks/BlockCounter.java
@@ -0,0 +1,227 @@
+package ZoneSeek.common.blocks;
+
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockPistonBase;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.dispenser.BehaviorDefaultDispenseItem;
+import net.minecraft.dispenser.IBlockSource;
+import net.minecraft.dispenser.IPosition;
+import net.minecraft.dispenser.IRegistry;
+import net.minecraft.dispenser.PositionImpl;
+import net.minecraft.dispenser.RegistryDefaulted;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntityDispenser;
+import net.minecraft.tileentity.TileEntityFurnace;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.Icon;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class BlockCounter extends Block
+{
+    /** Registry for all dispense behaviors. */
+    public static final IRegistry dispenseBehaviorRegistry = new RegistryDefaulted(new BehaviorDefaultDispenseItem());
+    protected Random random = new Random();
+    @SideOnly(Side.CLIENT)
+    protected Icon field_94463_c;
+    @SideOnly(Side.CLIENT)
+    protected Icon field_94462_cO;
+    @SideOnly(Side.CLIENT)
+    protected Icon field_96473_e;
+
+    protected BlockCounter(int par1)
+    {
+        super(par1, Material.rock);
+    }
+
+    /**
+     * How many world ticks before ticking
+     */
+    public int tickRate(World par1World)
+    {
+        return 4;
+    }
+
+    /**
+     * Called whenever the block is added into the world. Args: world, x, y, z
+     */
+    public void onBlockAdded(World par1World, int par2, int par3, int par4)
+    {
+        super.onBlockAdded(par1World, par2, par3, par4);
+        this.setCounterDefaultDirection(par1World, par2, par3, par4);
+    }
+
+    /**
+     * sets Dispenser block direction so that the front faces an non-opaque block; chooses west to be direction if all
+     * surrounding blocks are opaque.
+     */
+    private void setCounterDefaultDirection(World par1World, int par2, int par3, int par4)
+    {
+        if (!par1World.isRemote)
+        {
+            int l = par1World.getBlockId(par2, par3, par4 - 1);
+            int i1 = par1World.getBlockId(par2, par3, par4 + 1);
+            int j1 = par1World.getBlockId(par2 - 1, par3, par4);
+            int k1 = par1World.getBlockId(par2 + 1, par3, par4);
+            byte b0 = 3;
+
+            if (Block.opaqueCubeLookup[l] && !Block.opaqueCubeLookup[i1])
+            {
+                b0 = 3;
+            }
+
+            if (Block.opaqueCubeLookup[i1] && !Block.opaqueCubeLookup[l])
+            {
+                b0 = 2;
+            }
+
+            if (Block.opaqueCubeLookup[j1] && !Block.opaqueCubeLookup[k1])
+            {
+                b0 = 5;
+            }
+
+            if (Block.opaqueCubeLookup[k1] && !Block.opaqueCubeLookup[j1])
+            {
+                b0 = 4;
+            }
+
+            par1World.setBlockMetadataWithNotify(par2, par3, par4, b0, 2);
+        }
+    }
+
+    @SideOnly(Side.CLIENT)
+
+    /**
+     * From the specified side and block metadata retrieves the blocks texture. Args: side, metadata
+     */
+    public Icon getBlockTextureFromSideAndMetadata(int par1, int par2)
+    {
+    	return par1 == 1 ? this.field_94463_c : (par1 == 0 ? this.field_96473_e : (par1 != par2 ? this.blockIcon : this.field_94462_cO));
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IconRegister par1IconRegister)
+    {
+        this.blockIcon = par1IconRegister.registerIcon("zoneseek:counterside");
+        this.field_94463_c = par1IconRegister.registerIcon("zoneseek:granite");
+        this.field_94462_cO = par1IconRegister.registerIcon("zoneseek:countertop");
+        this.field_96473_e = par1IconRegister.registerIcon("zoneseek:hardwood");
+    }
+
+    /**
+     * Called upon block activation (right click on the block.)
+     */
+    public boolean onBlockActivated(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9)
+    {
+        if (par1World.isRemote)
+        {
+            return true;
+        }
+        else
+        {
+            TileEntityDispenser tileentitydispenser = (TileEntityDispenser)par1World.getBlockTileEntity(par2, par3, par4);
+
+            if (tileentitydispenser != null)
+            {
+                par5EntityPlayer.displayGUIDispenser(tileentitydispenser);
+            }
+
+            return null != null;
+        }
+    }
+
+
+
+    /**
+     * Lets the block know when one of its neighbor changes. Doesn't know which neighbor changed (coordinates passed are
+     * their own) Args: x, y, z, neighbor blockID
+     */
+    public void onNeighborBlockChange(World par1World, int par2, int par3, int par4, int par5)
+    {
+        boolean flag = par1World.isBlockIndirectlyGettingPowered(par2, par3, par4) || par1World.isBlockIndirectlyGettingPowered(par2, par3 + 1, par4);
+        int i1 = par1World.getBlockMetadata(par2, par3, par4);
+        boolean flag1 = (i1 & 8) != 0;
+
+        if (flag && !flag1)
+        {
+            par1World.scheduleBlockUpdate(par2, par3, par4, this.blockID, this.tickRate(par1World));
+            par1World.setBlockMetadataWithNotify(par2, par3, par4, i1 | 8, 4);
+        }
+        else if (!flag && flag1)
+        {
+            par1World.setBlockMetadataWithNotify(par2, par3, par4, i1 & -9, 4);
+        }
+    }
+
+    /**
+     * Ticks the block if it's been scheduled
+     */
+
+    /**
+     * Returns a new instance of a block's tile entity class. Called on placing the block.
+     */
+
+    /**
+     * Called when the block is placed in the world.
+     */
+    public void onBlockPlacedBy(World par1World, int par2, int par3, int par4, EntityLiving par5EntityLiving, ItemStack par6ItemStack)
+    {
+        int l = MathHelper.floor_double((double)(par5EntityLiving.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+
+        if (l == 0)
+        {
+            par1World.setBlockMetadataWithNotify(par2, par3, par4, 2, 2);
+        }
+
+        if (l == 1)
+        {
+            par1World.setBlockMetadataWithNotify(par2, par3, par4, 5, 2);
+        }
+
+        if (l == 2)
+        {
+            par1World.setBlockMetadataWithNotify(par2, par3, par4, 3, 2);
+        }
+
+        if (l == 3)
+        {
+            par1World.setBlockMetadataWithNotify(par2, par3, par4, 4, 2);
+        }
+
+    }
+
+    /**
+     * ejects contained items into the world, and notifies neighbours of an update, as appropriate
+     */
+
+    public static IPosition getIPositionFromBlockSource(IBlockSource par0IBlockSource)
+    {
+        EnumFacing enumfacing = getFacing(par0IBlockSource.getBlockMetadata());
+        double d0 = par0IBlockSource.getX() + 0.7D * (double)enumfacing.getFrontOffsetX();
+        double d1 = par0IBlockSource.getY() + 0.7D * (double)enumfacing.getFrontOffsetY();
+        double d2 = par0IBlockSource.getZ() + 0.7D * (double)enumfacing.getFrontOffsetZ();
+        return new PositionImpl(d0, d1, d2);
+    }
+
+    public static EnumFacing getFacing(int par0)
+    {
+        return EnumFacing.getFront(par0 & 7);
+    }
+
+    public boolean hasComparatorInputOverride()
+    {
+        return true;
+    }
+
+}
