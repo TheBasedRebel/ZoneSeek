dcf13dc2ae70b211c47e25aeb4061f59958b0697
Adam Bahr <thebasedrebel@gmail.com>
2013-03-27 19:51:45 -0400

    Is now tame-able, breed-able, and animated

--- ZoneSeek/common/models/ModelOrthrus.java
+++ ZoneSeek/common/models/ModelOrthrus.java
@@ -1,8 +1,12 @@
 package ZoneSeek.common.models;
 
+import ZoneSeek.common.entities.Orthrus;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.passive.EntityWolf;
+import net.minecraft.util.MathHelper;
 
 public class ModelOrthrus extends ModelBase
 {
@@ -18,21 +22,21 @@ public class ModelOrthrus extends ModelBase
     ModelRenderer Ear1;
     ModelRenderer Ear2;
     ModelRenderer Nose;
-    ModelRenderer WolfHead2;
-    ModelRenderer Nose2;
+    ModelRenderer WolfHead_2;
+    ModelRenderer Nose_2;
     ModelRenderer Ear3;
     ModelRenderer Ear4;
-    ModelRenderer Right_Wing_1;
-    ModelRenderer Right_Wing_2;
-    ModelRenderer Left_Wing_1;
-    ModelRenderer Left_Wing_2;
-    ModelRenderer R_Wing_1;
-    ModelRenderer R_Wing_2;
-    ModelRenderer L_Wing_1;
-    ModelRenderer Spike_1;
-    ModelRenderer Spike_2;
-    ModelRenderer Spike_3;
-    ModelRenderer R_Wing_3;
+    ModelRenderer Wing_R_1;
+    ModelRenderer Wing_R_2;
+    ModelRenderer Wing_R_3;
+    ModelRenderer Wing_R_4;
+    ModelRenderer Wing_L_1;
+    ModelRenderer Wing_L_2;
+    ModelRenderer Wing_L_3;
+    ModelRenderer Wing_L_4;
+    ModelRenderer Bone_1;
+    ModelRenderer Bone_2;
+    ModelRenderer Bone_3;
   
   public ModelOrthrus()
   {
@@ -46,8 +50,8 @@ public class ModelOrthrus extends ModelBase
       WolfHead.mirror = true;
       setRotation(WolfHead, 0F, 0F, 0F);
       Body = new ModelRenderer(this, 18, 14);
-      Body.addBox(-3F, -2F, -3F, 6, 9, 6);
-      Body.setRotationPoint(-1F, 14F, 2F);
+      Body.addBox(-4F, -2F, -3F, 6, 9, 6);
+      Body.setRotationPoint(0F, 14F, 2F);
       Body.setTextureSize(64, 64);
       Body.mirror = true;
       setRotation(Body, 1.570796F, 0F, 0F);
@@ -100,23 +104,23 @@ public class ModelOrthrus extends ModelBase
       Ear2.mirror = true;
       setRotation(Ear2, 0F, 0F, 0F);
       Nose = new ModelRenderer(this, 0, 10);
-      Nose.addBox(-5.5F, 0F, -5F, 3, 3, 4);
-      Nose.setRotationPoint(-1F, 13.5F, -7F);
+      Nose.addBox(-6F, 0F, -5F, 3, 3, 4);
+      Nose.setRotationPoint(-0.5F, 13.5F, -7F);
       Nose.setTextureSize(64, 64);
       Nose.mirror = true;
       setRotation(Nose, 0F, 0F, 0F);
-      WolfHead2 = new ModelRenderer(this, 0, 31);
-      WolfHead2.addBox(1F, -3F, -2F, 6, 6, 4);
-      WolfHead2.setRotationPoint(-1F, 13.5F, -7F);
-      WolfHead2.setTextureSize(64, 64);
-      WolfHead2.mirror = true;
-      setRotation(WolfHead2, 0F, 0F, 0F);
-      Nose2 = new ModelRenderer(this, 0, 41);
-      Nose2.addBox(2.5F, 0F, -5F, 3, 3, 4);
-      Nose2.setRotationPoint(-1F, 13.5F, -7F);
-      Nose2.setTextureSize(64, 64);
-      Nose2.mirror = true;
-      setRotation(Nose2, 0F, 0F, 0F);
+      WolfHead_2 = new ModelRenderer(this, 0, 31);
+      WolfHead_2.addBox(1F, -3F, -2F, 6, 6, 4);
+      WolfHead_2.setRotationPoint(-1F, 13.5F, -7F);
+      WolfHead_2.setTextureSize(64, 64);
+      WolfHead_2.mirror = true;
+      setRotation(WolfHead_2, 0F, 0F, 0F);
+      Nose_2 = new ModelRenderer(this, 0, 41);
+      Nose_2.addBox(2F, 0F, -5F, 3, 3, 4);
+      Nose_2.setRotationPoint(-0.5F, 13.5F, -7F);
+      Nose_2.setTextureSize(64, 64);
+      Nose_2.mirror = true;
+      setRotation(Nose_2, 0F, 0F, 0F);
       Ear3 = new ModelRenderer(this, 16, 42);
       Ear3.addBox(1F, -5F, 0F, 2, 2, 1);
       Ear3.setRotationPoint(-1F, 13.5F, -7F);
@@ -129,74 +133,73 @@ public class ModelOrthrus extends ModelBase
       Ear4.setTextureSize(64, 64);
       Ear4.mirror = true;
       setRotation(Ear4, 0F, 0F, 0F);
-      Right_Wing_1 = new ModelRenderer(this, 0, 51);
-      Right_Wing_1.addBox(2F, 0F, -5F, 7, 1, 1);
-      Right_Wing_1.setRotationPoint(-1F, 14F, 2F);
-      Right_Wing_1.setTextureSize(64, 64);
-      Right_Wing_1.mirror = true;
-      setRotation(Right_Wing_1, 0F, 0F, -0.7330383F);
-      Right_Wing_2 = new ModelRenderer(this, 0, 54);
-      Right_Wing_2.addBox(1F, -9F, -5F, 5, 1, 1);
-      Right_Wing_2.setRotationPoint(-1F, 14F, 2F);
-      Right_Wing_2.setTextureSize(64, 64);
-      Right_Wing_2.mirror = true;
-      setRotation(Right_Wing_2, 0F, 0F, 0.7330383F);
-      Left_Wing_1 = new ModelRenderer(this, 0, 51);
-      Left_Wing_1.addBox(2F, -2F, -5F, 7, 1, 1);
-      Left_Wing_1.setRotationPoint(-1F, 14F, 2F);
-      Left_Wing_1.setTextureSize(64, 64);
-      Left_Wing_1.mirror = true;
-      setRotation(Left_Wing_1, 0F, 0F, -2.303835F);
-      Left_Wing_2 = new ModelRenderer(this, 0, 54);
-      Left_Wing_2.addBox(1.2F, 8F, -5F, 5, 1, 1);
-      Left_Wing_2.setRotationPoint(-1F, 14F, 2F);
-      Left_Wing_2.setTextureSize(64, 64);
-      Left_Wing_2.mirror = true;
-      setRotation(Left_Wing_2, 0F, 0F, 2.452549F);
-      R_Wing_1 = new ModelRenderer(this, 21, 30);
-      R_Wing_1.addBox(1.6F, 0.5F, -5F, 7, 0, 8);
-      R_Wing_1.setRotationPoint(-1F, 14F, 2F);
-      R_Wing_1.setTextureSize(64, 64);
-      R_Wing_1.mirror = true;
-      setRotation(R_Wing_1, 0F, 0F, -0.7330383F);
-      R_Wing_2 = new ModelRenderer(this, 24, 39);
-      R_Wing_2.addBox(1.5F, -8.5F, -5F, 4, 0, 7);
-      R_Wing_2.setRotationPoint(-1F, 14F, 2F);
-      R_Wing_2.setTextureSize(64, 64);
-      R_Wing_2.mirror = true;
-      setRotation(R_Wing_2, 0F, 0F, 0.7330383F);
-      L_Wing_1 = new ModelRenderer(this, 21, 30);
-      L_Wing_1.addBox(1.6F, -1.5F, -5F, 7, 0, 8);
-      L_Wing_1.setRotationPoint(-1F, 14F, 2F);
-      L_Wing_1.setTextureSize(64, 64);
-      L_Wing_1.mirror = true;
-      setRotation(L_Wing_1, 0F, 0F, -2.303835F);
-      Spike_1 = new ModelRenderer(this, 18, 48);
-      Spike_1.addBox(-0.5F, -8F, -6F, 1, 5, 1);
-      Spike_1.setRotationPoint(-1F, 14F, 2F);
-      Spike_1.setTextureSize(64, 64);
-      Spike_1.mirror = true;
-      setRotation(Spike_1, 0F, 0F, 0F);
-      Spike_2 = new ModelRenderer(this, 18, 48);
-      Spike_2.addBox(-0.5F, -7F, -3F, 1, 5, 1);
-      Spike_2.setRotationPoint(-1F, 14F, 2F);
-      Spike_2.setTextureSize(64, 64);
-      Spike_2.mirror = true;
-      setRotation(Spike_2, 0F, 0F, 0F);
-      Spike_3 = new ModelRenderer(this, 18, 48);
-      Spike_3.addBox(-0.5F, -6F, 0F, 1, 5, 1);
-      Spike_3.setRotationPoint(-1F, 14F, 2F);
-      Spike_3.setTextureSize(64, 64);
-      Spike_3.mirror = true;
-      setRotation(Spike_3, 0F, 0F, 0F);
-      R_Wing_3 = new ModelRenderer(this, 24, 39);
-      R_Wing_3.addBox(1.6F, 8.5F, -5F, 4, 0, 7);
-      R_Wing_3.setRotationPoint(-1F, 14F, 2F);
-      R_Wing_3.setTextureSize(64, 64);
-      R_Wing_3.mirror = true;
-      setRotation(R_Wing_3, 0F, 0F, 2.452554F);
+      Wing_R_1 = new ModelRenderer(this, 0, 57);
+      Wing_R_1.addBox(1F, 3F, -5F, 2, 6, 1);
+      Wing_R_1.setRotationPoint(0F, 14F, 2F);
+      Wing_R_1.setTextureSize(64, 64);
+      Wing_R_1.mirror = true;
+      setRotation(Wing_R_1, 0F, 0F, -1.954769F);
+      Wing_R_2 = new ModelRenderer(this, 0, 49);
+      Wing_R_2.addBox(7F, -3F, -5F, 2, 5, 1);
+      Wing_R_2.setRotationPoint(0F, 14F, 2F);
+      Wing_R_2.setTextureSize(64, 64);
+      Wing_R_2.mirror = true;
+      setRotation(Wing_R_2, 0F, 0F, -0.3717861F);
+      Wing_R_3 = new ModelRenderer(this, 22, 31);
+      Wing_R_3.addBox(2.6F, -2F, -4F, 6, 1, 6);
+      Wing_R_3.setRotationPoint(0F, 14F, 2F);
+      Wing_R_3.setTextureSize(64, 64);
+      Wing_R_3.mirror = true;
+      setRotation(Wing_R_3, 0F, 0F, -0.4304457F);
+      Wing_R_4 = new ModelRenderer(this, 24, 39);
+      Wing_R_4.addBox(-2F, -8.5F, -2F, 4, 1, 4);
+      Wing_R_4.setRotationPoint(0F, 14F, 0F);
+      Wing_R_4.setTextureSize(64, 64);
+      Wing_R_4.mirror = true;
+      setRotation(Wing_R_4, 0F, 0F, 1.189716F);
+      Wing_L_1 = new ModelRenderer(this, 0, 57);
+      Wing_L_1.addBox(-2F, 5F, -5F, 2, 6, 1);
+      Wing_L_1.setRotationPoint(0F, 14F, 2F);
+      Wing_L_1.setTextureSize(64, 64);
+      Wing_L_1.mirror = true;
+      setRotation(Wing_L_1, 0F, 0F, 1.991947F);
+      Wing_L_2 = new ModelRenderer(this, 0, 49);
+      Wing_L_2.addBox(-11F, -2F, -5F, 2, 5, 1);
+      Wing_L_2.setRotationPoint(0F, 14F, 2F);
+      Wing_L_2.setTextureSize(64, 64);
+      Wing_L_2.mirror = true;
+      setRotation(Wing_L_2, 0F, 0F, 0.4089647F);
+      Wing_L_3 = new ModelRenderer(this, 22, 31);
+      Wing_L_3.addBox(-10.5F, -1.4F, -4F, 6, 1, 6);
+      Wing_L_3.setRotationPoint(0F, 14F, 2F);
+      Wing_L_3.setTextureSize(64, 64);
+      Wing_L_3.mirror = true;
+      setRotation(Wing_L_3, 0F, 0F, 0.4461433F);
+      Wing_L_4 = new ModelRenderer(this, 24, 39);
+      Wing_L_4.addBox(-1.6F, 9.4F, -4F, 4, 1, 4);
+      Wing_L_4.setRotationPoint(0F, 14F, 2F);
+      Wing_L_4.setTextureSize(64, 64);
+      Wing_L_4.mirror = true;
+      setRotation(Wing_L_4, 0F, 0F, 1.970466F);
+      Bone_1 = new ModelRenderer(this, 8, 49);
+      Bone_1.addBox(-1.5F, -8F, -6F, 1, 5, 1);
+      Bone_1.setRotationPoint(0F, 14F, 2F);
+      Bone_1.setTextureSize(64, 64);
+      Bone_1.mirror = true;
+      setRotation(Bone_1, 0F, 0F, 0F);
+      Bone_2 = new ModelRenderer(this, 8, 49);
+      Bone_2.addBox(-1.5F, -7F, -2F, 1, 5, 1);
+      Bone_2.setRotationPoint(0F, 14F, 2F);
+      Bone_2.setTextureSize(64, 64);
+      Bone_2.mirror = true;
+      setRotation(Bone_2, 0F, 0F, 0F);
+      Bone_3 = new ModelRenderer(this, 8, 49);
+      Bone_3.addBox(-1.5F, -6F, 2F, 1, 5, 1);
+      Bone_3.setRotationPoint(0F, 14F, 2F);
+      Bone_3.setTextureSize(64, 64);
+      Bone_3.mirror = true;
+      setRotation(Bone_3, 0F, 0F, 0F);
   }
-  
   public void render(Entity par1Entity, float par2, float par3, float par4, float par5, float par6, float par7)
   {
     super.render(par1Entity, par2, par3, par4, par5, par6, par7);
@@ -212,21 +215,21 @@ public class ModelOrthrus extends ModelBase
     Ear1.render(par7);
     Ear2.render(par7);
     Nose.render(par7);
-    WolfHead2.render(par7);
-    Nose2.render(par7);
+    WolfHead_2.render(par7);
+    Nose_2.render(par7);
     Ear3.render(par7);
     Ear4.render(par7);
-    Right_Wing_1.render(par7);
-    Right_Wing_2.render(par7);
-    Left_Wing_1.render(par7);
-    Left_Wing_2.render(par7);
-    R_Wing_1.render(par7);
-    R_Wing_2.render(par7);
-    L_Wing_1.render(par7);
-    Spike_1.render(par7);
-    Spike_2.render(par7);
-    Spike_3.render(par7);
-    R_Wing_3.render(par7);
+    Wing_R_1.render(par7);
+    Wing_R_2.render(par7);
+    Wing_R_3.render(par7);
+    Wing_R_4.render(par7);
+    Wing_L_1.render(par7);
+    Wing_L_2.render(par7);
+    Wing_L_3.render(par7);
+    Wing_L_4.render(par7);
+    Bone_1.render(par7);
+    Bone_2.render(par7);
+    Bone_3.render(par7);
   }
   
   private void setRotation(ModelRenderer model, float x, float y, float z)
@@ -235,12 +238,15 @@ public class ModelOrthrus extends ModelBase
     model.rotateAngleY = y;
     model.rotateAngleZ = z;
   }
+
   
   public void setRotationAngles(float par1, float par2, float par3, float par4, float par5, float par6, Entity par7Entity)
   {
       super.setRotationAngles(par1, par2, par3, par4, par5, par6, par7Entity);
       this.WolfHead.rotateAngleX = par5 / (180F / (float)Math.PI);
       this.WolfHead.rotateAngleY = par4 / (180F / (float)Math.PI);
+      this.WolfHead_2.rotateAngleX = par5 / (180F / (float)Math.PI);
+      this.WolfHead_2.rotateAngleY = par4 / (180F / (float)Math.PI);
       this.Ear1.rotateAngleX = par5 / (180F / (float)Math.PI);
       this.Ear1.rotateAngleY = par4 / (180F / (float)Math.PI);
       this.Ear2.rotateAngleX = par5 / (180F / (float)Math.PI);
@@ -251,9 +257,14 @@ public class ModelOrthrus extends ModelBase
       this.Ear4.rotateAngleY = par4 / (180F / (float)Math.PI);
       this.Nose.rotateAngleX = par5 / (180F / (float)Math.PI);
       this.Nose.rotateAngleY = par4 / (180F / (float)Math.PI);
-      this.Nose2.rotateAngleX = par5 / (180F / (float)Math.PI);
-      this.Nose2.rotateAngleY = par4 / (180F / (float)Math.PI);
-      this.Tail.rotateAngleX = par3;
+      this.Nose_2.rotateAngleX = par5 / (180F / (float)Math.PI);
+      this.Nose_2.rotateAngleY = par4 / (180F / (float)Math.PI);
+      this.Leg1.rotateAngleX = MathHelper.cos(par1 * 0.6662F) * 1.4F * par2;
+      this.Leg2.rotateAngleX = MathHelper.cos(par1 * 0.6662F + (float)Math.PI) * 1.4F * par2;
+      this.Leg3.rotateAngleX = MathHelper.cos(par1 * 0.6662F + (float)Math.PI) * 1.4F * par2;
+      this.Leg4.rotateAngleX = MathHelper.cos(par1 * 0.6662F) * 1.4F * par2;
+      this.Tail.rotateAngleY = MathHelper.cos(par3 * 0.3F + (float)par2 * 0.1F * (float)Math.PI) * (float)Math.PI * 0.01F * (float)(1 + Math.abs(par2 - 2));
+      this.Tail.rotationPointX = MathHelper.sin(par3 * 0.2F + (float)par2 * 0.1F * (float)Math.PI) * (float)Math.PI * 0.0F * (float)Math.abs(par2);
   }
 
 }
